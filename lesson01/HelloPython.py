040bea945b4b244d654b49c3a0a1786c58de29aa
vincentjava <vincentjava@yahoo.com.tw>
2019-05-11 10:06:08 +0800

    Python

---
+++ lesson01/HelloPython.py
@@ -0,0 +1,19 @@
+# -*- coding:UTF-8 -*-
+print("我是中文")
+
+h = 170 # int
+w = 60.5 # float
+x = 12649216492164919419822131241241221359275027357237529357982356982365326562352 # int -> long -> 無限(根據內存)
+
+print(type(h)) # 檢視資料型別
+print(type(w))
+print(type(x))
+
+age, name = 28, 'python'
+print(age, name)
+
+flag = True # True = 1, False = 0
+count = 5
+print(count + flag)
+
+
