58d20d438ad0e64b117f2b1cc944c295830cefc1
vincentjava <vincentjava@yahoo.com.tw>
2019-06-01 16:13:45 +0800

    Python

--- ip_fb.py
+++ ip_fb.py
@@ -1,6 +1,8 @@
 import socket
 import requests
 import json
+from firebase import firebase
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8",80))
@@ -11,4 +13,8 @@ print(ip)
 firebase_url = 'https://iotfb-fc0b9.firebaseio.com/'
 data = {'ip':ip}
 result = requests.put(firebase_url + '/raspberry.json', verify=False, data=json.dumps(data))
-print(result)
+
+
+# firebase = firebase.FirebaseApplication(firebase_url, None)
+# result = firebase.get('ip', None)
+# print(result)
